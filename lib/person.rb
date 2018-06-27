b06e0e8151c234fd04a33894607bf52c9dfca5b8
Andre <the.asantiagojr@gmail.com>
2018-06-27 17:02:43 -0400

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,86 @@
 # your code goes here
+class Person
+
+  attr_accessor :bank_account, :hygiene
+  attr_reader :name, :happiness
+  attr_writer :happiness
+
+  def initialize(name, bank_account=25, happiness=8, hygiene=8)
+    @name = name
+    @bank_account = bank_account
+    @happiness = happiness
+    @hygiene = hygiene
+  end
+
+  def happiness=(update)
+    @happiness = update
+    if @happiness > 10
+      @happiness = 10
+    elsif @happiness < 0
+      @happiness = 0
+    else
+      @happiness = update
+    end
+  end
+
+  def hygiene=(update)
+    @hygiene = update
+    if @hygiene > 10
+      @hygiene = 10
+    elsif @hygiene < 0
+      @hygiene = 0
+    else
+      @hygiene = update
+    end
+  end
+
+  def clean?
+    self.hygiene > 7
+  end
+
+  def happy?
+    self.happiness > 7
+  end
+
+  def get_paid(salary)
+    self.bank_account += salary
+    return "all about the benjamins"
+  end
+
+  def take_bath
+    self.hygiene += 4
+    return "♪ Rub-a-dub just relaxing in the tub ♫"
+  end
+
+  def work_out
+    self.hygiene -= 3
+    self.happiness += 2
+    return "♪ another one bites the dust ♫"
+  end
+
+  def call_friend(friend)
+    self.happiness += 3
+    friend.happiness += 3
+
+    return"Hi #{friend.name}! It's #{self.name}. How are you?"
+
+  end
+
+  def start_conversation(friend, topic)
+    if topic == 'politics'
+      self.happiness -= 2
+      friend.happiness -= 2
+      'blah blah partisan blah lobbyist'
+    elsif topic == 'weather'
+      self.happiness += 1
+      friend.happiness += 1
+      'blah blah sun blah rain'
+    else
+      'blah blah blah blah blah'
+    end
+  end
+
+end
+
+Bob = Person.new("Bob")
+# puts Bob.happiness
